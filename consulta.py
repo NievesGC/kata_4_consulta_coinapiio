0ac45bc44a3e472a3d923cf33c84c94249f8c14e
Nieves <niieves99@gmail.com>
2023-06-13 18:57:10 +0200

    primera consulta sin json()

---
+++ consulta.py
@@ -0,0 +1,10 @@
+import requests
+
+apikey = "1B731114-B858-433E-89F1-A792584BE95B"
+
+url = f"https://rest.coinapi.io/v1/exchangerate/BTC/EUR?apikey={apikey}"
+
+response = requests.get(url)
+
+print(response.status_code)
+print(response.text)
